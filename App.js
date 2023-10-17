2a5673a52522c145fd94cd1ff71939ffa4c92850
Leonardo Alarcon <leonardorh1718@gmail.com>
2023-10-17 17:30:35 -0500

    Firebase/Reactnative

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { cargarConfig } from "./app/utils/FirebaseConfig";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ProductoForm } from "./app/screens/ProductoForm";
+import { ListaProductos } from "./app/screens/ListaProductos";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
+  cargarConfig();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#f4511e",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="ListaProductosNav"
+          component={ListaProductos}
+          options={{
+            title: "Lista de Productos",
+          }}
+        />
+        <Stack.Screen
+          name="ProductoFormNav"
+          component={ProductoForm}
+          options={{
+            title: "Formulario Producto",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
